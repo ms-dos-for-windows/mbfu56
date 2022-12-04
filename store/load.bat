4c9ad03c8fc3a8561ea4833a0bf6928c7f5f7095
Maksim <45505876+ctv-software@users.noreply.github.com>
2022-12-04 17:11:01 +0300

    Add files via upload

---
+++ store/load.bat
@@ -0,0 +1,138 @@
+@echo off
+if "%1"=="yanbrow" Echo Downloading Yandex Browser
+if "%1"=="yanbrow" wget.exe  https://cachev2-mskmar03.cdn.yandex.net/download.yandex.ru/downloadable_soft/browser/15181/Yandex.exe
+if "%1"=="yanbrow" Yandex.exe
+
+
+
+if "%1"=="kaspersky" Echo Downloading Kaspersky antivirus
+if "%1"=="kaspersky" wget.exe https://bgkwur.securedfile.ru/b2/8/0/534e832353ccc493378393976140dfda/kis21.3.10.391ru_26488.exe
+if "%1"=="kaspersky" kis21.3.10.391ru_26488.exe
+
+
+
+if "%1"=="drwebc" Echo Downloading Dr.Web CureIt! 
+if "%1"=="drwebc" wget.exe https://ey7oiv.securedfile.ru/b4/8/2/5f039c21d9429abc2353924ce8f88475/cureit.exe
+if "%1"=="drwebc" cureit.exe
+
+
+if "%1"=="drweb" Echo Downloading Dr.Web
+if "%1"=="drweb" wget.exe https://51tbmz.securedfile.ru/b2/4/8/60f5c3d5a89c3c4a12967b74727b9f91/drweb-12.0-av-win.exe
+if "%1"=="drweb" drweb-12.0-av-win.exe
+
+
+if "%1"=="atom" Echo Downloading Atom Browser
+if "%1"=="atom" wget.exe https://6kh5bw.securedfile.ru/b6/9/6/24a31d49e75f6fe5ef854e0c4b2a8c38/atom.exe
+if "%1"=="atom" atom.exe
+
+if "%1"=="winrar" Echo Downloading WinRAR Archivaror
+if "%1"=="winrar" wget.exe https://acw6ut.securedfile.ru/b3/9/0/72cfb9a8b973fe300573914bdef99247/winrar-x32-611ru.exe
+if "%1"=="winrar" winrar-x32-611ru.exe
+
+if "%1"=="7ZIP" Echo Downloading 7ZIP Archivaror
+if "%1"=="7ZIP" wget.exe https://1t8ot8.securedfile.ru/b6/0/3/356d4200c1357459567a9f268ed7ad17/7z2201.exe
+if "%1"=="7ZIP" 7z2201.exe
+
+if "%1"=="akelpad" Echo Downloading AkelPad
+if "%1"=="akelpad" wget.exe https://fhd2fi.securedfile.ru/b5/2/3/fe716a0ecac268e4906ad871a223396b/AkelPad-4.9.8-setup.exe
+if "%1"=="akelpad" AkelPad-4.9.8-setup.exe
+
+if "%1"=="bin2inc" Echo Downloading Bin2Inc
+if "%1"=="bin2inc" wget.exe https://github.com/ctv-software/BIN2INC/raw/main/Bin2Inc_0021_SetUp.exe
+if "%1"=="bin2inc" Bin2Inc_0021_SetUp.exe
+
+if "%1"=="host" Echo Downloading HomeStudio
+if "%1"=="host" wget.exe https://3gcqrs.securedfile.ru/b9/4/5/b1dd8e1fb5a40bf3e4636636d88c56fe/HomeStudio.exe
+if "%1"=="host" HomeStudio.exe
+
+if "%1"=="yadisk" Echo Downloading Yandex Disk
+if "%1"=="yadisk" wget.exe https://2apk.ru/windows/YandexDiskSetup.exe
+if "%1"=="yadisk" YandexDiskSetup.exe
+
+
+if "%1"=="obloco" Echo Downloading Obloco Mail.ru
+if "%1"=="obloco" wget.exe https://2apk.ru/windows/Mail.Ru_Cloud.exe
+if "%1"=="obloco" Mail.Ru_Cloud.exe
+
+if "%1"=="r7office" Echo Downloading r7-office
+if "%1"=="r7office" wget.exe https://download.r7-office.ru/windows/r7office_x86.exe
+if "%1"=="r7office" r7office_x86.exe
+
+if "%1"=="farman" Echo Downloading FAR Manager
+if "%1"=="farman" wget.exe https://e8s38s.securedfile.ru/b9/6/5/288f93eae102baad09fb3cb1c1ccb2bc/Far30b5959.x86.20220202.msi
+if "%1"=="farman" Far30b5959.x86.20220202.msi
+
+
+
+if "%1"=="myoffice" Echo Downloading My office
+if "%1"=="myoffice" wget.exe https://preset.myoffice-app.ru/MyOfficeStandardHomeEdition.msi
+if "%1"=="myoffice" MyOfficeStandardHomeEdition.msi
+
+
+if "%1"=="Rsaver" Echo Downloading R.saver
+if "%1"=="Rsaver" wget.exe https://rlab.ru/download/R.saver.zip
+if "%1"=="Rsaver" R.saver.zip
+
+if "%1"=="Punto" Echo Downloading PuntoSwitcher
+if "%1"=="Punto" wget.exe https://ext-cachev2-itt02.cdn.yandex.net/download.cdn.yandex.net/punto/PuntoSwitcherSetup.exe
+if "%1"=="Punto" PuntoSwitcherSetup.exe
+
+
+if "%1"=="AIMP" Echo Downloading AIMP
+if "%1"=="AIMP" wget.exe https://50hbuy.securedfile.ru/b6/7/0/2f2b7320b80f0e7da8c3cd3acd619a7e/aimp_5.10.2414_w32.exe
+if "%1"=="AIMP" aimp_5.10.2414_w32.exe
+
+if "%1"=="mbfu" Echo Downloading MS-DOS 6.22 BOOT FROM USB
+if "%1"=="mbfu" wget.exe https://github.com/ctv-software/MBFU/raw/main/MBFU56.exe
+if "%1"=="mbfu" MBFU56.exe
+
+
+if "%1"=="mfw" Echo Downloading MS-DOS 6.22 FOR WINDOWS
+if "%1"=="mfw" wget.exe https://github.com/ctv-software/mfw/raw/main/MSDOS622U.exe
+if "%1"=="mfw" MSDOS622U.exe
+
+if "%1"=="sput"  Echo Downloading Sputnik Browser
+if "%1"=="sput" wget.exe https://apv0iq.securedfile.ru/b5/3/2/6b4babf6c640a095a465b85756ffab15/sputnik_setup_full.exe
+if "%1"=="sput" sputnik_setup_full.exe
+
+if "%1"=="kotatogram"  Echo Downloading Kotatogram
+if "%1"=="kotatogram" wget.exe https://github.com/kotatogram/kotatogram-desktop/releases/download/k1.4.8/1.4.8-win32-installer.exe
+if "%1"=="kotatogram" 1.4.8-win32-installer.exe
+
+if "%1"=="MPDFE" Echo Downloading Master PDF Editor
+if "%1"=="MPDFE" wget.exe https://code-industry.ru/public/MasterPDFEditor-setup-ru.exe
+if "%1"=="MPDFE" MasterPDFEditor-setup-ru.exe
+
+if "%1"=="double" Echo Downloading Double Commander
+if "%1"=="double" wget.exe https://3fotwf.securedfile.ru/b2/1/4/311421de856d82778aab4badc10cf18c/doublecmd-1.0.8.i386-win32.exe
+if "%1"=="double" doublecmd-1.0.8.i386-win32.exe
+
+if "%1"=="VEAMS" Echo Downloading Video Editor
+if "%1"=="VEAMS" wget.exe https://amssoft.ru/downloads/VideoEditorAMS.exe
+if "%1"=="VEAMS" VideoEditorAMS.exe
+
+
+if "%1"=="fotosh" Echo Downloading Foto Show
+if "%1"=="fotosh" wget.exe https://amspark.ru/FotoShow.exe
+if "%1"=="fotosh" FotoShow.exe
+
+
+if "%1"=="screncam" Echo Downloading Screen Camera
+if "%1"=="screncam" wget.exe https://5hjif9.securedfile.ru/b2/3/7/461e160e61bd1b0bc300dc8058a82a86/ScreenCamera.exe
+if "%1"=="screncam" ScreenCamera.exe
+
+if "%1"=="Victoria" Echo Downloading Victoria537
+if "%1"=="Victoria" wget.exe https://ae5sxe.securedfile.ru/b9/2/6/324f150630ce42170646ddd6a4501cd2/Victoria537.zip
+if "%1"=="Victoria" Victoria537.zip
+
+if "%1"=="nanoav" Echo Downloading NANO Antivirus
+if "%1"=="nanoav" wget.exe https://ar3mzl.securedfile.ru/b9/5/6/48ab4d99481e6f8aa461f614e086d528/nanoav.free.setup.exe
+if "%1"=="nanoav" nanoav.free.setup.exe
+
+if "%1"=="avz" Echo Downloading AVZ Antivirus
+if "%1"=="avz" wget.exe https://yfw2rd.securedfile.ru/b5/3/3/82205bde5e3a3ea5480dd92099975941/avz4.zip
+if "%1"=="avz" avz4.zip
+
+if "%1"=="uran" Echo Downloading Uran Browser
+if "%1"=="uran" wget.exe https://2l7mj6.securedfile.ru/b4/3/1/6e1ee96c4ceb8b44c0d8bd6f7b06e8e2/uransetup.exe
+if "%1"=="uran" uransetup.exe
